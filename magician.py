68eb46836a1a5792e0c6c9ee512ecef27d54b9c1
OlivierBigonnesse <OlivierBigonnesse@users.noreply.github.com>
2020-11-26 18:25:55 -0500

    thing

--- magician.py
+++ magician.py
@@ -16,12 +16,18 @@ from weapon_user import *
 class Magician(WeaponUser, Spellcaster):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
+		self.using_magic = False
 
-	# TODO: Surcharger la méthode `compute_damage` 
+	# TODO: Surcharger la méthode `compute_damage`
+	def compute_damage(self, other):
 		# Si on veut et peut utiliser de la magie (`will_use_spell()`):
+		if self.will_use_spell():
 			# TODO: Utiliser le `compute_damage` de `Spellcaster`
+			return Spellcaster.compute_damage(self, other)
 		# Sinon
+		else:
 			# TODO: Utiliser le `compute_damage` de `WeaponUser`
+			return WeaponUser.compute_damage(self, other)
 
 	def will_use_spell(self):
 		return self.using_magic and self.can_use_spell()
